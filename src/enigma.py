968d21ee942bf7483b7eac39c2300606c641f41b
vtarale <66897674+vtarale@users.noreply.github.com>
2022-02-25 13:33:17 +0530

    Update enigma.py

--- src/enigma.py
+++ src/enigma.py
@@ -2,7 +2,7 @@ import random
 
 class Rotor1:
     def __init__(self):
-        self.alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'j', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     
     def move(self):
         last = self.alphabets[0]
